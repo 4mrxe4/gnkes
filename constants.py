47ef2d5be88c463493fa35b3a596f75e81cd76ff
ggxxzz3773-web <ggxxzz3773@gmail.com>
2026-09-01 22:09:36 +0300

    تحديث من البوت

---
+++ constants.py
@@ -0,0 +1,18 @@
+
+from compat import (
+    ChatType,
+    ChatMemberStatus,
+    ParseMode,
+    ChatAction,
+    MessageEntityType,
+    ChatMembersFilter,
+)
+
+__all__ = [
+    "ChatType",
+    "ChatMemberStatus",
+    "ParseMode",
+    "ChatAction",
+    "MessageEntityType",
+    "ChatMembersFilter",
+]
